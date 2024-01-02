26cb4cb2c65820e46dc5d59aa8cc30f224490e4e
chuthanh1310 <chuthanh1310@gmail.com>
2024-01-03 04:27:18 +0700

    UX/UI

---
+++ UX-UI/src/sample/helpController.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+public class helpController {
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    void handleClose(ActionEvent event) {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+
+        
+        stage.close();
+    }
+
+}
